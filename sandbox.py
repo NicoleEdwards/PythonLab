8317c2e916cf2907cfb69cca689139e5f8421c32
nicoleedwards <nicole.edwards722@gmail.com>
2015-03-06 00:33:54 +0000

    question1

---
+++ sandbox.py
@@ -0,0 +1,29 @@
+x = 5
+y = 3
+z = y + x
+print z
+
+z = 5 / 3 # divide two integers, get an integer back
+print z
+z = 5.0 / 3 # divide a float by an integer, get a float back
+print z
+
+x = 5.0
+y = 3.0
+z = x/y
+y = 5.0
+print z
+
+x = 5.0
+y = ( x**2 + 5*x + 2 )/10.0
+print y
+
+subject = "Billy Jean"
+predicate = "is not my lover"
+sentence = subject + " " + predicate
+print sentence
+
+subject = "Ke$ha\""
+predicate = "is my bff4eva!!! <3"
+sentence = subject + " " + predicate
+print sentence
